65016281310b4011db78390207d631cdc554d033
Josh <karta.josh.natin@gmail.com>
2024-04-14 12:40:23 -0300

    feat: add projects block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,31 @@ import BentobBlock from "@/blocs/bento-block";
 import { HeroBlock } from "@/blocs/hero-block";
 import MarqueeBlock from "@/blocs/marquee-block";
 import NewsBlock from "@/blocs/news-block";
+import { ProjectsBlock } from "@/blocs/projects-block";
 
 export default function Home() {
 
   return (
     <main className="flex flex-col lg:pt-20">
 
+      {/* Hero */}
       <div className="w-full" id="home">
         <HeroBlock />
       </div>
 
+      {/* About */}
       <div className="w-full" id="about">
         <AboutBlock />
       </div>
 
+      {/* Services */}
       <div className="w-full" id="services">
         <BentobBlock />
+      </div>
+
+      {/* Projects */}
+      <div className="pb-16">
+        <ProjectsBlock />
       </div>
 
       {/* Pricing */}
