21b48f46b8cf18cf589fab40afb2c07a088c7bfa
Josh <karta.josh.natin@gmail.com>
2024-07-13 20:52:35 -0300

    feat: implement smoothscroll and react query changed to fetching data on clientside using react query

---
+++ src/actions/fetchSanity.ts
@@ -0,0 +1,12 @@
+import { query, sanityClient } from "@/lib/sanity"
+
+export async function getData() {
+    try {
+          const data = await sanityClient.fetch(query)
+
+return data
+
+    } catch (error) {
+        return {error}
+    }
+}
--- src/app/(Index)/page.tsx
+++ src/app/(Index)/page.tsx
@@ -1,24 +1,46 @@
+"use client"
 import AboutBlock from "@/blocs/about-block";
 import { HeroBlock } from "@/blocs/hero-block";
 import { ExperienceBlock } from "@/blocs/experiences-block";
 import ProjectsBlock from "@/blocs/projects-block";
 import ContactBlock from "@/blocs/contact-block";
-import { query, sanityClient } from "@/lib/sanity";
+import { useEffect } from "react";
+import Lenis from 'lenis'
+import { useQuery } from "@tanstack/react-query";
+import { getData } from "@/actions/fetchSanity";
 
-export default async function Home() {
-  const { hero, about, experience, experienceProjects, projects } = await sanityClient.fetch(query)
+export default function Home() {
+  const { data, isFetched } = useQuery({
+    queryKey: [
+      "data"
+    ],
+    queryFn: getData
+  })
 
-  return (
+  useEffect(() => {
+    const lenis = new Lenis()
+    lenis.on('scroll', (e: any) => {
+      console.log(e)
+    })
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+  }, [])
+
+
+  if (isFetched) return (
     <main className="flex flex-col bg-primary-two-500">
 
       {/* Hero */}
       <div className="w-full" id="hero">
-        <HeroBlock data={hero} />
+        <HeroBlock data={data.hero} />
       </div>
 
       {/* About */}
       <div className="w-full" id="about">
-        <AboutBlock data={about} />
+        <AboutBlock data={data.about} />
       </div>
 
       {/* Services */}
@@ -28,12 +50,12 @@ export default async function Home() {
 
       {/* Experience */}
       <div className="pb-16" id="experience">
-        <ExperienceBlock experience={experienceProjects} experienceSection={experience} />
+        <ExperienceBlock experience={data.experienceProjects} experienceSection={data.experience} />
       </div>
 
       {/* Projects */}
       <div className="w-full py-16 bg-primary-two-600" id="projects">
-        <ProjectsBlock projects={projects} />
+        <ProjectsBlock projects={data.projects} />
       </div>
 
       {/* Marquees */}
