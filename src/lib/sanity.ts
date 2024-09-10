78ed295c2abd6909904dd4c858b90884ec7254af
Josh <karta.josh.natin@gmail.com>
2024-09-10 07:47:24 -0300

    feat: modify query to fetch data

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,24 +1,26 @@
 import { createClient } from "next-sanity";
-import imageUrlBuilder from '@sanity/image-url'
+import imageUrlBuilder from "@sanity/image-url";
 
 export const sanityClient = createClient({
-    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    useCdn: false
-})
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  useCdn: false,
+});
 
-const imgBuilder = imageUrlBuilder(sanityClient)
+const imgBuilder = imageUrlBuilder(sanityClient);
 
-export const urlFor = (source:any) => {
- return imgBuilder.image(source)
-}
+export const urlFor = (source: any) => {
+  return imgBuilder.image(source);
+};
 
-const queryHeroSection = `*[_type== "heroSection"] {badge, name, profilePicture}[0]`
-const queryAboutSection = `*[_type== "about"] {title, description, image}[0]`
-const queryExperienceSection = `*[_type== "experienceSection"] {title, description}[0]`
-const queryExperience = `*[_type== "experience"] {title, link, thumbnail}`
-const queryProjects = `*[_type== "project"] {name, introduction, description, url, thumbnail, mockup, mockupType}`
+const queryHeroSection = `*[_type== "heroSection"] {badge, name, profilePicture}[0]`;
+const queryAboutSection = `*[_type== "about"] {title, description, image}[0]`;
+const queryExperienceSection = `*[_type== "experienceSection"] {title, description}[0]`;
+const queryExperience = `*[_type== "experience"] {title, link, thumbnail}`;
+const queryProjects = `*[_type== "project"] {name, introduction, description, url, thumbnail, mockup, mockupType}`;
+const queryJourney = `*[_type== "journey"] {title, date, role}`;
+const querySkills = `*[_type== "skill"] {skill}`;
 
 export const query = `{
   "hero": ${queryHeroSection},
@@ -26,4 +28,6 @@ export const query = `{
   "experience": ${queryExperienceSection},
   "experienceProjects": ${queryExperience},
   "projects": ${queryProjects},
-}`
+  "journey": ${queryJourney},
+  "skills": ${querySkills},
+}`;
